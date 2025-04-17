270a07d7a5ea015fd8b2ed164022f119f37b1890
happyrio <happyrio1013@gmail.com>
2025-04-17 20:51:00 +0900

    fix: bot message

--- src/components/botMessage.tsx
+++ src/components/botMessage.tsx
@@ -5,7 +5,7 @@ const BotMessage = ({ text }: { text: string }) => {
   return (
     <div className="flex rounded-lg space-x-2.5">
       <img src={botImg} alt="bot" width={38} height={38} className="self-start" />
-      <div className="p-2 text-white text-sm bg-[#101F2E] rounded-lg px-4 py-2 min-h-14 w-72 flex items-center">
+      <div className="p-2 text-white text-sm bg-[#101F2E] rounded-lg px-4 py-2 min-h-14 w-72 flex flex-col justify-center">
         <ReactMarkdown
           components={{
             a: ({ node, ...props }) => (
